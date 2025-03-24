f4d13c48184486e944400167beed5dd25e482bb9
MrFish486 <laszlodeanpav@gmail.com>
2025-03-24 16:37:41 -0700

    Finished (yay!)

--- lib.js
+++ lib.js
@@ -4,15 +4,22 @@ class board {
 		this.board = board;
 		this.cache = cache;
 		this.size = {x : board[0].length, y : board.length};
+		this.onmerge = (x, y)=>{};
 	}
 	whatis(x, y){
+		if(x < 0 || y < 0 || x > this.size.x - 1 || y > this.size.y - 1){
+			return 1;
+		}
 		return this.board[x][y];
 	}
 	set(x, y, val){
 		return this.board[x][y] = val;
 	}
-	render(canvas){
+	render(canvas, grayscale){
 		let c = canvas.getContext("2d");
+		if(grayscale){
+			c.filter = "grayscale(1)";
+		}
 		// Compute scale:
 		let xs = canvas.width / this.size.x;
 		let ys = canvas.height / this.size.y;
@@ -41,6 +48,152 @@ class board {
 		}
 	}
 	go(dir){
-		//
+		switch(dir){
+			case "u":
+				while(true){
+					let cp = JSON.stringify(this.board);
+					for(let x = 0; x < this.size.x; x++){
+						for(let y = 0; y < this.size.y; y++){
+							if(this.whatis(x - 1, y) == 0){
+								let pre = this.whatis(x, y);
+								this.set(x, y, 0);
+								this.set(x - 1, y, pre);
+							}
+						}
+					}
+					if(cp == JSON.stringify(this.board)){
+						break;
+					}
+				}
+				break;
+			case "d":
+				while(true){
+					let cp = JSON.stringify(this.board);
+					for(let x = 0; x < this.size.x; x++){
+						for(let y = 0; y < this.size.y; y++){
+							if(this.whatis(x + 1, y) == 0){
+								let pre = this.whatis(x, y);
+								this.set(x, y, 0);
+								this.set(x + 1, y, pre);
+							}
+						}
+					}
+					if(cp == JSON.stringify(this.board)){
+						break;
+					}
+				}
+				break;
+			case "l":
+				while(true){
+					let cp = JSON.stringify(this.board);
+					for(let x = 0; x < this.size.x; x++){
+						for(let y = 0; y < this.size.y; y++){
+							if(this.whatis(x, y - 1) == 0){
+								let pre = this.whatis(x, y);
+								this.set(x, y, 0);
+								this.set(x, y - 1, pre);
+							}
+						}
+					}
+					if(cp == JSON.stringify(this.board)){
+						break;
+					}
+				}
+				break;
+			case "r":
+				while(true){
+					let cp = JSON.stringify(this.board);
+					for(let x = 0; x < this.size.x; x++){
+						for(let y = 0; y < this.size.y; y++){
+							if(this.whatis(x, y + 1) == 0){
+								let pre = this.whatis(x, y);
+								this.set(x, y, 0);
+								this.set(x, y + 1, pre);
+							}
+						}
+					}
+					if(cp == JSON.stringify(this.board)){
+						break;
+					}
+				}
+				break;
+			default:
+				console.error(`Can't go in direction ${v}!`)
+				break;
+		}
+	}
+	haswon(){
+		for(let x = 0; x < this.size.x; x++){
+			for(let y = 0; y < this.size.y; y++){
+				if(this.whatis(x, y) == 2048){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	merge(dir){
+		switch(dir){
+			case "u":
+				for(let x = 0; x < this.size.x; x++){
+					for(let y = 0; y < this.size.y; y++){
+						if(this.whatis(x - 1, y) == this.whatis(x, y)){
+							this.onmerge(x, y);
+							this.set(x, y, 0);
+							this.set(x - 1, y, this.whatis(x - 1, y) * 2);
+						}
+					}
+				}
+				break;
+			case "d":
+				for(let x = 0; x < this.size.x; x++){
+					for(let y = 0; y < this.size.y; y++){
+						if(this.whatis(x + 1, y) == this.whatis(x, y)){
+							this.onmerge(x, y);
+							this.set(x, y, 0);
+							this.set(x + 1, y, this.whatis(x + 1, y) * 2);
+						}
+					}
+				}
+				break;
+			case "l":
+				for(let x = 0; x < this.size.x; x++){
+					for(let y = 0; y < this.size.y; y++){
+						if(this.whatis(x, y - 1) == this.whatis(x, y)){
+							this.onmerge(x, y);
+							this.set(x, y, 0);
+							this.set(x, y - 1, this.whatis(x, y - 1) * 2);
+						}
+					}
+				}
+				break;
+			case "r":
+				for(let x = 0; x < this.size.x; x++){
+					for(let y = 0; y < this.size.y; y++){
+						if(this.whatis(x, y + 1) == this.whatis(x, y)){
+							this.onmerge(x, y);
+							this.set(x, y, 0);
+							this.set(x, y + 1, this.whatis(x, y + 1) * 2);
+						}
+					}
+				}
+				break;
+			default:
+				console.error(`Can't go in direction ${v}!`)
+				break;
+		}
+	}
+	haslost(){
+		for(let x = 0; x < this.size.x; x++){
+			for(let y = 0; y < this.size.y; y++){
+				if(this.whatis(x, y) == 0){
+					return false;
+				}
+				if(this.whatis(x - 1, y) == this.whatis(x, y) || this.whatis(x + 1, y) == this.whatis(x, y) || this.whatis(x, y - 1) == this.whatis(x, y) || this.whatis(x, y + 1) == this.whatis(x, y)){
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 }
--- run.js
+++ run.js
@@ -11,5 +11,58 @@ var empty = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]];
 var game = new board(JSON.parse(JSON.stringify(empty)), defaults);
 
 setInterval(()=>{
-	game.render(document.getElementById("main"));
+	if(game.haswon()){
+		document.getElementById("status").innerHTML = "You won!";
+	}else if(game.haslost()){
+		document.getElementById("status").innerHTML = "You lost!";
+		game.render(document.getElementById("main"), true);
+	}else{
+		document.getElementById("status").innerHTML = "Score : " + score;
+	}
+}, 50);
+
+game.salt();
+game.salt();
+setTimeout(()=>{game.render(document.getElementById("main"))}, 50);
+var score = 0;
+
+game.onmerge = (x, y)=>{
+	score += game.whatis(x, y) * 2;
+	document.getElementById("status").classList.add("_slam");
+	setTimeout(()=>{document.getElementById("status").classList.remove("_slam")}, 251);
+}
+
+document.addEventListener("keydown", e=>{
+	switch(e.code){
+		case "ArrowUp":
+			game.go("u");
+			game.merge("u");
+			game.go("u");
+			game.salt();
+			game.render(document.getElementById("main"), game.haslost());
+			break;
+		case "ArrowDown":
+			game.go("d");
+			game.merge("d");
+			game.go("d");
+			game.salt();
+			game.render(document.getElementById("main")), game.haslost();
+			break;
+		case "ArrowLeft":
+			game.go("l");
+			game.merge("l");
+			game.go("l");
+			game.salt();
+			game.render(document.getElementById("main")), game.haslost();
+			break;
+		case "ArrowRight":
+			game.go("r");
+			game.merge("r");
+			game.go("r");
+			game.salt();
+			game.render(document.getElementById("main")), game.haslost();
+			break;
+		default:
+			break;
+	}
 });
